51e18463c87dcb8feed890af121b5f5d83bac271
Gayathri <gayathrikaveri04@gmail.com>
2026-08-24 16:06:56 +0530

    second commit from gayathri branch

--- src/test/java/Practice/sample.java
+++ src/test/java/Practice/sample.java
@@ -4,6 +4,9 @@ public class sample {
 	public static void main(String[] args) {
 		
 		System.out.println("learning github");
+		System.out.println("Status");
+		System.out.println("Branch");
+		System.out.println("Remote");
 		
 	}
 
